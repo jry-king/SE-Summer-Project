40be0dcfc934538c4d65d524250fd9099f5740d7
jry-king <2472559327@qq.com>
2018-07-23 17:12:34 +0800

    switch from flask to spring

---
+++ code/reid-backend/src/main/java/com/reidbackend/demo/ReidRun.java
@@ -0,0 +1,11 @@
+package com.reidbackend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReidRun {
+	public static void main(String[] args) {
+		SpringApplication.run(ReidRun.class, args);
+	}
+}
