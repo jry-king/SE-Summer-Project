12c56baf5cdbcf6fa9f72cefe35098d3a37600ee
zzbslayer <zzbslayer@sjtu.edu.cn>
2018-07-23 17:13:44 +0800

    map management

--- code/clever-eye-frontend/src/Management/MapManagement.js
+++ code/clever-eye-frontend/src/Management/MapManagement.js
@@ -1,21 +1,17 @@
 import React, {Component} from 'react'
 import { dataApi } from '../Global'
 import MapRow from './MapRow'
-import { message, Button, Icon, Upload } from 'antd'
-
-function getBase64(img, callback) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.readAsDataURL(img);
-  }
+import { message, Button, Input, Row, Col } from 'antd'
 
 class MapManagement extends Component{
 
     constructor(props){
         super(props)
         this.state = {
-            mapString: null,
-            maps:null
+            maps:null,
+
+            areaid:null,
+            map:null
         }
         this.getMap()
     }
@@ -42,73 +38,8 @@ class MapManagement extends Component{
         )
     }
 
-    beforeUpload = (file) => {
 
-        const isLt5M = file.size / 1024 / 1024 < 5;
-        if (!isLt5M) {
-            message.error('Image must smaller than 5MB!');
-        }
-
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-
-        reader.onloadend =  () => {
-            this.setState({mapString: reader.result});
-        }    
-        return false;
-    }
-
-    handleUpload = (e) => {
-        e.preventDefault();
-        if (this.state.mapString === null){
-            message.error("upload error.")
-            return
-        }
-        let json = {"areaid": 0, "map": this.state.mapString}
-
-        fetch( dataApi+"map/save", {
-            method:'post',
-            credentials:'include',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify(json)
-        })
-        .then(res => res.json())
-        .then(
-            (result) => {
-                if (result.status)
-                    message.error(result.message)
-                else{
-                    this.setState({
-                        mapString: null
-                    });
-                    message.success('upload successfully.');
-                }
-            },
-            (error) => {
-                console.log()
-                message.error(error)
-            }
-        )
-      }
-
-    handleChange = (info) => {
-        if (info.file.status === 'uploading') {
-          this.setState({ loading: true });
-          return;
-        }
-        if (info.file.status === 'done') {
-          // Get this url from response in real world.
-          getBase64(info.file.originFileObj, imageUrl => this.setState({
-            imageUrl,
-            loading: false,
-          }));
-        }
-      }
-
-      deleteMap = (mapid) => {
+    deleteMap = (mapid) => {
         fetch(dataApi + "map/delete?mapid=" + mapid,{
             method:'get',
             credentials: 'include'
@@ -137,58 +68,93 @@ class MapManagement extends Component{
         )
     }
 
+    handleSave = () => {
+        if (this.state.map!=="" &&this.state.areaid!=="" ) {
+            let msg = {
+                "mapid":0,
+                "map":this.state.map,
+                "areaid":this.state.areaid
+            }
+
+            fetch(dataApi+"map/save", {
+                method: 'post',
+                credentials: 'include',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(msg)
+
+            })
+            .then(res => res.json())
+            .then(
+                (result)=>{
+                    if (result.status){
+                        message.error("Save Error")
+                        console.log(result.message)
+                    }
+                    else {
+                        message.success("Save Success")
+                        this.setState({map:null,areaid:null})
+                    }
+                },
+                (error) => {
+                    message.error("Network Error")
+                    console.log(error)
+                }
+            )
+        }
+    }
+
     render(){
-        let mapString = this.state.mapString
-
         let maps = this.state.maps
-        const uploadButton = (
-            <div>
-              <Icon type={this.state.loading ? 'loading' : 'plus'} />
-              <div className="ant-upload-text">Upload</div>
-            </div>
-          );
-
+        let map = this.state.map
+        let areaid = this.state.areaid
         return(            
 			<div>
-				<div className="avatar-uploader">
-					<Upload
-						name="map"
-						listType="picture-card"
-						showUploadList={false}
-						beforeUpload={this.beforeUpload}
-						onChange={this.handleChange}
-					>
-						{
-                            mapString ? <img src={mapString} alt="map" /> : uploadButton
-                        }
-                </Upload>
-				<br/>
-				</div>
 				<div>
-					<Button color="primary" type="submit" onClick={this.handleUpload}>Submit</Button>
-				</div>
+                    <Row>
+                        <Col span={3}/>
+                        <Col span={4}>
+                        <Input type="text" defaultValue={areaid} placeholder="Areaid" onChange={this.handleChange} name="areaid"/>
+                        </Col>
+                        <Col span={1}/>
+                        <Col span={9}>
+                        <Input type="text" defaultValue={map} placeholder="Map" onChange={this.handleChange} name="map"/>
+                        </Col>
+                        <Col span={3}>
+                        <Button onClick={this.handleSave}>Save</Button>
+                        </Col>
+                    </Row>
+                </div>
                 <br/>
-                <div width="900">
-                <table className="ant-table ant-table-bordered" style={{margin:'auto'}}>
-                <thead className="ant-table-thead">
-                    <tr>
-                        <th>Mapid</th>
-                        <th>Areaid</th>  
-                        <th>Map</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody className="ant-table-tbody">
-                    {
-                        maps?maps.map((m) => {
-                            return(
-                                <MapRow key={m.mapid} map={m} deleteMap={this.deleteMap}/>
-                            )
-                        }):null
-                    }
-                </tbody>
-                </table>
+                <div>
+                <Row>
+                    <Col span={3}/>
+                    <Col span={16}>
+                    <table className="ant-table ant-table-bordered">
+                    <thead className="ant-table-thead">
+                        <tr>
+                            <th width={200}>Mapid</th>
+                            <th width={300}>Map</th>  
+                            <th width={200}>Areaid</th>
+                            <th width={200}>Edit</th>
+                            <th width={200}>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody className="ant-table-tbody">
+                        {
+                            maps?maps.map((m) => {
+                                return(
+                                    <MapRow key={m.mapid} map={m} deleteMap={this.deleteMap}/>
+                                )
+                            }):null
+                        }
+                    </tbody>
+                    </table>
+                    </Col>
+                    <Col span={3}/>
+                </Row>
                 </div>
 			</div>
         )
