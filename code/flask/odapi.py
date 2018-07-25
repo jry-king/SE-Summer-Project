0bcc01b943bd380408916607a3964a75c6b8ae37
jry-king <2472559327@qq.com>
2018-07-25 11:32:49 +0800

    integrated version of odapi

--- code/flask/odapi.py
+++ code/flask/odapi.py
@@ -8,10 +8,9 @@ import json
 import re
 import time
 import subprocess as sp
-import base64
-from io import BytesIO
-import csv
-UPLOAD_FOLDER = '/Users/darlenelee/Documents/vir_env/models/research/object_detection/upload'
+from tripletreid.reid import calcreid
+
+UPLOAD_FOLDER = 'F:/SEclasses/SEintro/project/research/object_detection/upload'
 WEBURL = "http://47.106.8.44:8080/"
 VIDEO_URL = WEBURL + "live/camera2.m3u8"
 app = Flask(__name__)
@@ -40,51 +39,68 @@ class JsonEncoder(json.JSONEncoder):
             return super(JsonEncoder, self).default(obj)
 
 
-@app.route("/receiver", methods=['POST'])
-def receive():
-    postValues= request.form.get("img")
-    image_data = re.sub('^data:image/.+;base64,', '', postValues)
-    im = Image.open(BytesIO(base64.b64decode(image_data)))
-    im.save('query.jpg')
-    csvfile = open('query.csv','a')
-    writer = csv.writer(csvfile)
-    writer.writerow([1,'query.jpg'])
-    csvfile.close()
-    return json.dumps({'result': 'success'}), 200, {'ContentType': 'application/json'}
+@app.route("/upload", methods=['POST'])
+def hello():
+    while True:
+        print("test----")
+        pipe = sp.Popen(["ffmpeg", "-i", VIDEO_URL,
+                         "-loglevel", "quiet", # no text output
+                         "-an",   # disable audio
+                         "-f", "image2pipe",
+                         "-pix_fmt", "bgr24",
+                         "-vcodec", "rawvideo", "-"],
+                        stdin=sp.PIPE, stdout=sp.PIPE)
+        while True:
+            raw_image = pipe.stdout.read(1280*720*3)
+            image = np.fromstring(raw_image, dtype='uint8')
+            image = np.array(image).reshape(1280, 720, 3)
+            print(len(image))
+            print(len(image[0]))
+            # result = odapi_server.detect(image)
+            break
 
+    # file = request.files['file']
+    # file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    # file.save(file_path)
     
-@app.route("/stream", methods=['GET','POST'])
+    # file = Image.open(file)
+    # file = np.array(file)
+    # result = odapi_server.run_inference_for_single_image(file)
+    return json.dumps(result, cls=JsonEncoder)
+
+
+@app.route("/stream", methods=['GET'])
 def video():
-    postValues= request.form.get("img")
-    image_data = re.sub('^data:image/.+;base64,', '', postValues)
-    im = Image.open(BytesIO(base64.b64decode(image_data)))
-    im.save('query.jpg')
-    csvfile = open('query.csv','wb')
-    writer = csv.writer(csvfile)
-    writer.writerow([1,'query.jpg'])
-    csvfile.close()
     index = 0
     while True:
         print("test----")
-        pipe = sp.Popen([ "ffmpeg", "-i", VIDEO_URL,
-                "-loglevel", "quiet", # no text output
-                "-an",   # disable audio
-                "-f", "image2pipe",
-                "-pix_fmt", "bgr24",
-                "-vcodec", "rawvideo", "-"],
-                stdin = sp.PIPE, stdout = sp.PIPE)
-        index = index+1
+        pipe = sp.Popen(["ffmpeg", "-i", VIDEO_URL,
+                         "-loglevel", "quiet",  # no text output
+                         "-an",   # disable audio
+                         "-f", "image2pipe",
+                         "-pix_fmt", "bgr24",
+                         "-vcodec", "rawvideo", "-"],
+                        stdin=sp.PIPE, stdout=sp.PIPE)
+        index = index + 1
+
         while True:
             raw_image = pipe.stdout.read(1280*720*3)
-            image =  np.fromstring(raw_image, dtype='uint8')
-            image = np.array(image).reshape(720,1280,3)
-            result = odapi_server.detect(image,'gallery.csv',index)
+            image = np.fromstring(raw_image, dtype='uint8')
+            image = np.array(image).reshape(720, 1280, 3)
+            result = odapi_server.detect(image, 'gallery.csv', index)
             print(result)
+            calcreid("tripletreid\\experiment", "query.csv", "gallery.csv", "query", "gallery",
+                     "queryembeddings.h5", "galleryembeddings.h5")
             break
+
+    # file = Image.open(file)
+    # file = np.array(file)
+    # result = odapi_server.run_inference_for_single_image(file)
     return json.dumps(result, cls=JsonEncoder)
+
 
 if __name__ == '__main__':
     port = 5000
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
-    app.run(host='0.0.0.0',port = port)
+    app.run(host='0.0.0.0', port=port)
