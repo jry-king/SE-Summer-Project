0e897f7a4dc3e0bdffd162aef3b9dde62cb200cd
zzbslayer <zzbslayer@sjtu.edu.cn>
2018-08-09 15:19:02 +0800

    more data

--- code/gets-backend/sql/data.sql
+++ code/gets-backend/sql/data.sql
@@ -3,6 +3,9 @@ insert into cameras values (2,"param1","param2","param3","30%","40%",1);
 insert into cameras values (3,"param1","param2","param3","35%","68%",2);
 insert into cameras values (4,"param1","param2","param3","63%","23%",2);
 
-insert into history values (0,1,1,"test");
-insert into history values (0,1,1,"test2");
-insert into history values (0,2,1,"test3");
+insert into history values (1,1,1,"test");
+insert into history values (2,1,1,"test2");
+insert into history values (3,2,1,"test3");
+insert into history values (4,2,1,"test4");
+insert into history values (5,3,2,"test5");
+insert into history values (6,4,2,"test6");
